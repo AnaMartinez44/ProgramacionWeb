a53553795a5dd78872012f64e25fb6bb19f57915
AnaMartinez44 <anamartinez89@aragon.unam.mx>
2023-11-28 23:00:15 -0800

    se le aplico un fondo violeta a la página

---
+++ EliminarUsuario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 style=" text-align: center;" >Elimina a un usuario</h1>
+    <form style=" text-align: center;" method="POST" action="deleteUsuario.php">
+
+        <input  type="text" name="no_cuenta" placeholder="Numero de Cuenta" />
+        <br />
+        <button type="submit">Eliminar usuario</button>
+
+    </form>
+    
+    <h1><a href="index.php">Inicio de listas</a></h1>
+
+</body>
+
+</html>
