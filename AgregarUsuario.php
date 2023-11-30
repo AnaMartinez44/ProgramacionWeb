44cdb605ef1fc1c7afaf66cd78d6b3e5b61224cc
AnaMartinez44 <anamartinez89@aragon.unam.mx>
2023-11-30 14:37:47 -0800

    etiqueta para que el ususario pueda resgistrarse

--- AgregarUsuario.php
+++ AgregarUsuario.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar a un usuario </title>
 </head>
-<body> 
+<body style="background-color: #aa8ab3;">
     <div>
     <form method= "post" action="AgregarUsuario.php" name=lolo>
 
     <table>
-        <tr><td style="background-color: #aa8ab3;"> <label> Registrar</label></td></tr>
+        <tr><td style="background-color: #aa8ab3;"> <label> Registrarte</label></td></tr>
+        <tr><td><input type="text" name= "Ingresa tu usuario"/> </td></tr>
