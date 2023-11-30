af120130dc073ece9d84beb0377fea0e1b30b689
AnaMartinez44 <anamartinez89@aragon.unam.mx>
2023-11-29 21:33:37 -0800

    se ajusto cada sección a una altura y centrada

--- EliminarUsuario.php
+++ EliminarUsuario.php
@@ -4,22 +4,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
+    
+    <title>Elimina a un usuario </title>
+    <style>
+        #seccion1 {
+            height: 200px;
+        }
+        h1 {
+            text-align: center;
+        }
+
+            #seccion2 {
+            height: 300px;
+            }
+
+            #seccion3 {
+            height: 300px;
+            }     
+    </style>
+
 </head>
 
-<body>
-    <h1 style=" text-align: center;" >Elimina a un usuario</h1>
+<body style="background-color: #aa8ab3;">
+    <div id="seccion1">
+    <h1> Eliminar usuario</h1>
+    </div>
+    
+    <div id="seccion2">
     <form style=" text-align: center;" method="POST" action="deleteUsuario.php">
 
+    
         <input  type="text" name="no_cuenta" placeholder="Numero de Cuenta" />
         <br />
         <button type="submit">Eliminar usuario</button>
-
+        </div>
     </form>
-    
-    <h1><a href="index.php">Inicio de listas</a></h1>
-
+    <div id="seccion3">
+    <h1><a href="index.php">Inicio de lista</a></h1>
+    </div>
 </body>
 
 </html>
