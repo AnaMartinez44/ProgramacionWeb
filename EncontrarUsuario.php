3fbfed94f6ca27ae3a7496dd05c48fb49292b7aa
AnaMartinez44 <anamartinez89@aragon.unam.mx>
2023-11-30 17:58:40 -0800

    se le agrego el logo de la fees

--- EncontrarUsuario.php
+++ EncontrarUsuario.php
@@ -14,8 +14,75 @@
         }
        .cabecera {
         background-image: url("https://shorturl.at/uPUX1");
+        color: #fff;
+            padding: 10px;
+            text-align: center;
+            display: flex;
+            align-items: center;
+        }
+
+        .logo{
+           margin-right: 10px;  
+        }
+        .texto {
+            flex-grow: 1;
+            text-align: center;
+        }
+
+
+        #formularioContainer {
+            max-width: 400px;
+            margin: auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 20px;
+        }
+
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+        }
+
+
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 16px;
+            box-sizing: border-box;
+        }
+
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            cursor: pointer;
+        }
+
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+<body style="background-color: #a18594;">
+<body>
+    <div class = "cabecera">
+        <div class = "logo">
+            <img 
+            src="https://amei.mx/wp-content/uploads/2016/08/UNAM-FES-Aragon.png" 
+            alt="Logo" 
+            height="100">
+
+
+       
+
+
+
         
-    } 
+    
 
         
 
